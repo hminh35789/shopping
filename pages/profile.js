b6c79270cf28f405db694dc1da85e40ecabd98e8
hminh35789 <hminh35789@gmail.com>
2022-06-09 00:23:23 +0700

    reset password

--- pages/profile.js
+++ pages/profile.js
@@ -4,8 +4,8 @@ import { useState, useContext, useEffect } from 'react'
 import { DataContext } from '../store/GlobalState'
 import Link from 'next/link'
 
-// import valid from '../utils/valid'
-// import { patchData } from '../utils/fetchData'
+import valid from '../utils/valid'
+import { patchData } from '../utils/fetchData'
 
 // import {imageUpload} from '../utils/imageUpload'
 
@@ -27,6 +27,71 @@ const Profile = () => {
         if(auth.user) setData({...data, name: auth.user.name})
     // eslint-disable-next-line react-hooks/exhaustive-deps
     },[auth.user])
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setData({...data, [name]:value})
+        dispatch({ type: 'NOTIFY', payload: {} })
+    }
+
+    const handleUpdateProfile = e => {
+        e.preventDefault()
+        if(name === ''|| cf_password === '' || password === ''){
+            const errMsg = valid(name, auth.user.email, password, cf_password)
+            
+            if(errMsg) return dispatch({ type: 'NOTIFY', payload: {error: errMsg} })
+            // updatePassword()
+        }
+        if(password || name){
+            const errMsg = valid(name, auth.user.email, password, cf_password)
+            console.log(errMsg)
+            if(errMsg) return dispatch({ type: 'NOTIFY', payload: {error: errMsg} })
+            updatePassword()
+        }
+
+        // if(name !== auth.user.name || avatar) updateInfor()
+    }
+    const updatePassword = () => {
+        dispatch({ type: 'NOTIFY', payload: {loading: true} })
+        patchData('user/resetPassword', {password}, auth.token)
+        .then(res => {
+            if(res.err) return dispatch({ type: 'NOTIFY', payload: {error: res.err} })
+            return dispatch({ type: 'NOTIFY', payload: {success: res.msg} })
+        })
+    }
+
+    // const changeAvatar = (e) => {
+    //     const file = e.target.files[0]
+    //     if(!file)
+    //         return dispatch({type: 'NOTIFY', payload: {error: 'File does not exist.'}})
+
+    //     if(file.size > 1024 * 1024) //1mb
+    //         return dispatch({type: 'NOTIFY', payload: {error: 'The largest image size is 1mb.'}})
+
+    //     if(file.type !== "image/jpeg" && file.type !== "image/png") //1mb
+    //         return dispatch({type: 'NOTIFY', payload: {error: 'Image format is incorrect.'}})
+        
+    //     setData({...data, avatar: file})
+    // }
+
+    // const updateInfor = async () => {
+    //     let media;
+    //     dispatch({type: 'NOTIFY', payload: {loading: true}})
+
+    //     if(avatar) media = await imageUpload([avatar])
+
+    //     patchData('user', {
+    //         name, avatar: avatar ? media[0].url : auth.user.avatar
+    //     }, auth.token).then(res => {
+    //         if(res.err) return dispatch({type: 'NOTIFY', payload: {error: res.err}})
+
+    //         dispatch({type: 'AUTH', payload: {
+    //             token: auth.token,
+    //             user: res.user
+    //         }})
+    //         return dispatch({type: 'NOTIFY', payload: {success: res.msg}})
+    //     })
+    // }
 
     if(!auth.user) return null;
     return(
@@ -57,21 +122,21 @@ const Profile = () => {
                         <label htmlFor="name">Name</label>
                         <input type="text" name="name" value={name} className="form-control"
                         placeholder="Your name" 
-                        // onChange={handleChange}
+                        onChange={handleChange}
                          />
                     </div>
 
                     <div className="form-group">
                         <label htmlFor="email">Email</label>
                         <input type="text" name="email" defaultValue={auth.user.email} 
-                        className="form-control" disabled={true} />
+                        className="form-control" disabled={true}  />
                     </div>
 
                     <div className="form-group">
                         <label htmlFor="password">New Password</label>
                         <input type="password" name="password" value={password} className="form-control"
                         placeholder="Your new password" 
-                        // onChange={handleChange}
+                        onChange={handleChange}
                          />
                     </div>
 
@@ -79,13 +144,13 @@ const Profile = () => {
                         <label htmlFor="cf_password">Confirm New Password</label>
                         <input type="password" name="cf_password" value={cf_password} className="form-control"
                         placeholder="Confirm new password"
-                        //  onChange={handleChange}
+                         onChange={handleChange}
                          />
                     </div>
 
                     <button className="btn btn-info"
                      disabled={notify.loading}
-                    // // onClick={handleUpdateProfile}
+                     onClick={handleUpdateProfile}
                     >
                         Update
                     </button>
